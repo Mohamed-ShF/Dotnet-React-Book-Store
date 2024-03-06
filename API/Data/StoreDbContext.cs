881d135a2da4741bda165407516c8abeaebd9f2a
Mohamed42002 <mohamed4_2002@hotmail.com>
2024-03-06 09:24:03 +0200

    Initialize project and adding (Product, Product Controller, DBContext) Classes

---
+++ API/Data/StoreDbContext.cs
@@ -0,0 +1,15 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class StoreDbContext:DbContext
+    {
+        public StoreDbContext(DbContextOptions dbContextOptions):base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Product> Products{get; set;}
+    }
+}
